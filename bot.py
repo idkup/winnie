db92f899887d219701a9439da26aca881aa68880
idkup <idkup137@gmail.com>
2022-01-19 18:03:23 -0500

    hc

--- bot.py
+++ bot.py
@@ -161,23 +161,14 @@ async def evspots(ctx):
     await ctx.send("https://cdn.discordapp.com/attachments/404811373546176514/549088567603888128/image0.jpg")
 
 
-@bot.command()
-async def fudzillabistirdmebzergingvarie(ctx):
-    await ctx.author.send("What the fuck did you just fucking say about me, you little bitch? \
-    I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret \
-    raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in \
-    the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with \
-    precision the likes of which has never been seen before on this Earth, mark my fucking words. \
-    You think you can get away with saying that shit to me over the Internet? Think again, fucker. \
-    As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you \
-    better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. \
-    You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s \
-    just with my bare hands. Not only am I extensively trained in unarmed combat, \
-    but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to \
-    wipe your miserable ass off the face of the continent, you little shit. \
-    If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you,\
-     maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, \
-     you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.")
+@bot.command(aliases=["hc"])
+async def headcount(ctx, dungeon):
+    if ctx.author.guild_permissions.manage_messages:
+        e = discord.Embed(title=f"Headcount for {dungeon}",
+                          description=f"React with {bot.get_emoji(925313325221437441)} to participate!")
+        message = await ctx.channel.send(f"Headcount started by {ctx.author.mention}.", embed=e)
+        await ctx.send(message)
+        await message.add_reaction(bot.get_emoji(925313325221437441))
 
 
 @bot.command()
