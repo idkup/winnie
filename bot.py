5f3dc0dfc80b6264ecbd5d1551ef7ea94aad9481
idkup <idkup137@gmail.com>
2025-04-06 05:17:47 -0400

    add source

--- bot.py
+++ bot.py
@@ -506,7 +506,7 @@ async def on_raw_reaction_add(payload):
                     e = discord.Embed()
                     e.set_author(name=message.author.name)
                     e.description = message.content
-                    await channel.send("asdf")
+                    e.add_field(name="Source", value=f"[Jump!]({message.jump_link})")
                     await bot.get_channel(STARBOARD_CHANNELS[guild.id]).send(embed=e)
                 break
 
